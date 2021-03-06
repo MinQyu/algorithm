be2cccf11046204cae2a30d3a1422b3f1cf96005
MinQyu <jorushs@gmail.com>
2021-03-06 21:41:03 +0900

    백준 10952) A+B - 5

---
+++ BOJ/BOJ10952.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "H.h"
+using namespace std;
+void BOJ10952()
+{
+	int A, B;
+	while (1)
+	{
+		cin >> A >> B;
+		if (A != 0)
+		{
+			cout << A + B << "\n";
+		}
+		else
+		{
+			break;
+		}
+	}
+}
