90b237c4d0bcee10eb54a53e41ce8bdca9161bfe
MinQyu <78997415+MinQyu@users.noreply.github.com>
2023-12-28 19:41:17 +0900

    기초/n 번째 원소까지(수정)
    .slice() 이용, 코드 수정

--- programmersJS/n 번째 원소까지.js
+++ programmersJS/n 번째 원소까지.js
@@ -1,7 +1,4 @@
 function solution(num_list, n) {
-    let answer = [];
-    for(i = 0; i < n; i++){
-        answer.push(num_list[i]);
-    }
+    const answer = num_list.slice(0, n);
     return answer;
 }
