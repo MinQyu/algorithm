07aa2d815c7995b72ebf695f7868caf23715adef
MinQyu <78997415+MinQyu@users.noreply.github.com>
2024-01-10 18:47:07 +0900

    Update 원소들의 곱과 합.js

--- programmersJS/원소들의 곱과 합.js
+++ programmersJS/원소들의 곱과 합.js
@@ -1,12 +1,4 @@
 function solution(num_list) {
-    let mul = num_list[0];
-    let pl = 0;
-    for(i=1; i<num_list.length; i++){
-        mul *= num_list[i];
-    }
-    for(i=0; i<num_list.length; i++){
-        pl += num_list[i];
-    }
-    const answer = mul < pl**2 ? 1 : 0;
+    const answer = num_list.reduce((acc, cur) => acc * cur, 1) < num_list.reduce((acc, cur) => acc + cur, 0)**2 ? 1 : 0;
     return answer;
 }
