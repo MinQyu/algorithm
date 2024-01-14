6ba072e46072d28620cae8af4219a8b7a6bb7a9e
MinQyu <78997415+MinQyu@users.noreply.github.com>
2024-01-14 21:15:42 +0900

    Update 부분 문자열 이어 붙여 묹자열 만들기.js

--- programmersJS/부분 문자열 이어 붙여 묹자열 만들기.js
+++ programmersJS/부분 문자열 이어 붙여 묹자열 만들기.js
@@ -1,7 +1,7 @@
 function solution(my_strings, parts) {
-    let answer = '';
+    const answer = [];
     for(i=0; i<my_strings.length; i++){
-        answer += my_strings[i].slice(parts[i][0], parts[i][1]+1);
+        answer.push(my_strings[i].slice(parts[i][0], parts[i][1]+1));
     }
-    return answer;
+    return answer.join('');
 }
