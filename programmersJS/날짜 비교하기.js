2a73960ee5727145f150b1b32a1946d959f4dcd6
MinQyu <78997415+MinQyu@users.noreply.github.com>
2024-01-18 16:16:45 +0900

    Update 날짜 비교하기.js

--- programmersJS/날짜 비교하기.js
+++ programmersJS/날짜 비교하기.js
@@ -1,11 +1,5 @@
 function solution(date1, date2) {
-    let answer = 0;
-    if(date1[0] < date2[0]) {
-        answer = 1;
-    } else if(date1[0] === date2[0] && date1[1] < date2[1]) {
-        answer = 1;
-    } else if(date1[1] === date2[1] && date1[2] < date2[2]) {
-        answer = 1;
-    }
-    return answer;
+    const d1 = new Date(date1);
+    const d2 = new Date(date2);
+    return d1 < d2 ? 1 : 0;
 }
