7b4e711d426a049bfa4d61d3e2d80ec22827809e
MinQyu <78997415+MinQyu@users.noreply.github.com>
2024-01-30 18:11:08 +0900

    Update 가위 바위 보.js

--- programmersJS/가위 바위 보.js
+++ programmersJS/가위 바위 보.js
@@ -1,3 +1,9 @@
 function solution(rsp) {
-    return rsp.split('').map(e => e === '2' ? '0' : (e === '0' ? '5' : '2')).join('');
+    const r = '0', s = '2', p = '5';
+    const win = (e) => {
+        if(e === r) return p;
+        if(e === s) return r;
+        if(e === p) return s;
+    }
+    return rsp.split('').map(e => win(e)).join('');
 }
