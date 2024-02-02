30e36769956acfa326a7b2aa51159efc630db2b8
MinQyu <78997415+MinQyu@users.noreply.github.com>
2024-02-02 17:10:39 +0900

    Update 암호 해독.js

--- programmersJS/암호 해독.js
+++ programmersJS/암호 해독.js
@@ -1,3 +1,3 @@
 function solution(cipher, code) {
-    return cipher.split('').filter((e, i) => (i + 1) % code === 0 ? e : null).join('');
+    return cipher.split('').filter((e, i) => (i + 1) % code === 0).join('');
 }
