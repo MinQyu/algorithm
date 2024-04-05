1d0cbae27d7d3c869ee29b4cd9d5739d494eb287
MinQyu <jorushs@gmail.com>
2024-04-06 00:09:42 +0900

    Update 숨어있는 숫자의 덧셈.js

--- programmersJS/숨어있는 숫자의 덧셈.js
+++ programmersJS/숨어있는 숫자의 덧셈.js
@@ -1,3 +1,3 @@
 function solution(my_string) {
-    return my_string.split(/[a-z]/i).reduce((acc, cur) => acc + Number(cur), 0);
+  return my_string.split(/[^0-9]/i).reduce((acc, cur) => acc + Number(cur), 0);
 }
