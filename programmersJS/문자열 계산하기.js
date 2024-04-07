e945711e304b1613067753cdf430a262d5814871
MinQyu <jorushs@gmail.com>
2024-04-07 17:14:54 +0900

    Update 문자열 계산하기.js

--- programmersJS/문자열 계산하기.js
+++ programmersJS/문자열 계산하기.js
@@ -1,10 +1,14 @@
 function solution(my_string) {
-    return my_string.split(' ').reduce((acc, cur, i, arr) => {
-        if (cur == '+') {
-            acc = Number(acc) + Number(arr[i + 1]);
-        } else if (cur == '-') {
-            acc = Number(acc) - Number(arr[i + 1]);
-        }
-        return acc;
-    });
+  return my_string.split(" ").reduce((acc, cur, i, arr) => {
+    if (i == 0) {
+      acc += Number(cur);
+    } else {
+      if (cur == "+") {
+        acc += Number(arr[i + 1]);
+      } else if (cur == "-") {
+        acc -= Number(arr[i + 1]);
+      }
+    }
+    return acc;
+  }, 0);
 }
