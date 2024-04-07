0442ec00a6e60bd290f709efbe90531d4498f484
MinQyu <jorushs@gmail.com>
2024-04-07 15:36:47 +0900

    Update 한 번만 등장한 문자.js

--- programmersJS/한 번만 등장한 문자.js
+++ programmersJS/한 번만 등장한 문자.js
@@ -1,3 +1,9 @@
 function solution(s) {
-    return s.split('').sort().filter((e, i, arr) => arr.filter(k => k == e).length === 1).join('');
+  return s
+    .split("")
+    .sort()
+    .reduce((acc, cur, i, arr) => {
+      if (i == 0) return cur != arr[i + 1] ? acc + cur : acc;
+      else return cur != arr[i - 1] && cur != arr[i + 1] ? acc + cur : acc;
+    }, "");
 }
