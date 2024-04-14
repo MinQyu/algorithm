352c0b819e46b641f863b2a67e9e3bf7bb41fb60
MinQyu <jorushs@gmail.com>
2024-04-14 19:32:17 +0900

    Update 옹알이.js

--- programmersJS/옹알이.js
+++ programmersJS/옹알이.js
@@ -1,9 +1,4 @@
 function solution(babbling) {
-    const bab = ["aya", "ye", "woo", "ma"];
-    for (i = 0; i < babbling.length; i++) {
-        for (j = 0; j < 4; j++) {
-            babbling[i].includes(bab[j]) ? babbling[i] = babbling[i].replace(bab[j], 1) : null;
-        }
-    }
-    return babbling.filter(e => Number(e)).length;
+  const regex = /^(aya|ye|woo|ma)+$/;
+  return babbling.filter((e) => regex.test(e)).length;
 }
