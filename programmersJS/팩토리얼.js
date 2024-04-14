d2a41cf3290610e61c95832ef409e50d91203c11
MinQyu <jorushs@gmail.com>
2024-04-14 17:39:14 +0900

    Update 팩토리얼.js

--- programmersJS/팩토리얼.js
+++ programmersJS/팩토리얼.js
@@ -1,10 +1,10 @@
 function solution(n) {
-  const arr = Array.from({ length: 11 }, (_, i) => i + 1);
-  let acc = 1;
-  for (i = 0; i < 11; i++) {
-    acc *= arr[i];
-    if (acc > n) {
-      return i;
-    }
-  }
+  const ref = Array.from({ length: 11 }, (_, i) => i + 1);
+  let answer = 0;
+  ref.reduce((acc, cur, i) => {
+    acc *= cur;
+    if (acc > n && answer == 0) answer = i;
+    return acc;
+  });
+  return answer;
 }
