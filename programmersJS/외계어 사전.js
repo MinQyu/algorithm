4116cb313933b7d0d6c6711c92191e5196eb456b
MinQyu <jorushs@gmail.com>
2024-04-16 15:45:13 +0900

    forEach for로 수정

--- programmersJS/외계어 사전.js
+++ programmersJS/외계어 사전.js
@@ -2,10 +2,11 @@ function solution(spell, dic) {
   let answer = 2;
   const getSortedString = (arr) => arr.sort().join("");
   const sortedSpell = getSortedString(spell);
-  dic.forEach((e) => {
-    if (getSortedString(e.split("")) === sortedSpell) {
+  for (i = 0; i < dic.length; i++) {
+    if (getSortedString(dic[i].split("")) === sortedSpell) {
       answer = 1;
+      break;
     }
-  });
+  }
   return answer;
 }
