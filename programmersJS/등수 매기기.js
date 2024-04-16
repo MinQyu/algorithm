6cc27d6cad4c9beed6b73684c625d90c6d534e2c
MinQyu <jorushs@gmail.com>
2024-04-16 16:42:22 +0900

    Update 등수 매기기.js

--- programmersJS/등수 매기기.js
+++ programmersJS/등수 매기기.js
@@ -1,5 +1,5 @@
 function solution(score) {
-    score = score.map(e => (e[0] + e[1]) / 2);
-    const avg = [...score].sort((a, b) => b - a);
-    return score.map(e => avg.indexOf(e) + 1);
+  const averageScore = score.map((e) => (e[0] + e[1]) / 2);
+  const sortedAverageScore = [...averageScore].sort((a, b) => b - a);
+  return averageScore.map((e) => sortedAverageScore.indexOf(e) + 1);
 }
