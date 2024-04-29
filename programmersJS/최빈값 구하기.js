ea158fd0b89868d3375c6e37a236603338d81440
MinQyu <jorushs@gmail.com>
2024-04-30 01:58:58 +0900

    Update 최빈값 구하기.js

--- programmersJS/최빈값 구하기.js
+++ programmersJS/최빈값 구하기.js
@@ -1,6 +1,28 @@
 function solution(array) {
-    array = array.map((e, i, arr) => [e, arr.filter(n => e === n).length])
-                 .sort((a, b) => b[1] - a[1])
-                 .filter((e, i, arr) => e[1] === arr[0][1]);
-    return array.reduce((acc, cur) => cur[0] !== array[0][0] ? acc = -1 : acc, array[0][0]);
+  const getFrequencyMap = (array) => {
+    const frequencyMap = new Map();
+    array.forEach((key) => {
+      const currentValue = frequencyMap.get(key);
+      if (currentValue) {
+        frequencyMap.set(key, currentValue + 1);
+      } else {
+        frequencyMap.set(key, 1);
+      }
+    });
+    return frequencyMap;
+  };
+  const getModeValues = (frequencyMap) => {
+    const frequencyArray = Array.from(frequencyMap);
+    frequencyArray.sort((a, b) => b[1] - a[1]);
+    const maxFrequency = frequencyArray[0][1];
+    return frequencyArray.reduce(
+      (acc, [value, frequency]) =>
+        frequency === maxFrequency ? acc.concat(value) : acc,
+      []
+    );
+  };
+  const frequencyMap = getFrequencyMap(array);
+  const modeValues = getModeValues(frequencyMap);
+
+  return modeValues.length === 1 ? modeValues[0] : -1;
 }
